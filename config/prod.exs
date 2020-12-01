c0ac9d3a102b694cdb17865a4f5050754f112a87
marciio camello <mac3designer@gmail.com>
2020-12-01 12:01:51 -0300

    Deploy

--- config/prod.exs
+++ config/prod.exs
@@ -12,6 +12,7 @@ use Mix.Config
 config :twitter_clone_elixir, TwitterCloneElixirWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
