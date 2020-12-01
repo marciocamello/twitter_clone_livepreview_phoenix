fa667b681c6b82e2c8b4ad6915867d3983cd0ddd
marciio camello <mac3designer@gmail.com>
2020-12-01 12:08:03 -0300

    Deploy

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :twitter_clone_elixir, TwitterCloneElixirWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "lucky-reflecting-junebug.gigalixirapp.com/", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
